cf899aed2efad39955da36473f5a2d4f9f7c209e
muratariku0903 <18ba179r@rikkyo.ac.jp>
2022-07-24 03:15:12 +0900

    add ListLessThan10

---
+++ tasks/ListLessThan10.dart
@@ -0,0 +1,13 @@
+void main(List<String> args) {
+  List<int> nums = [1, 2, 3, 4, 5, 6];
+
+  // for (var num in nums) {
+  //   if (num < 5) {
+  //     print(num);
+  //   }
+  // }
+
+  for (var i = 0; i < nums.length; i++) {
+    print(nums[i]);
+  }
+}
