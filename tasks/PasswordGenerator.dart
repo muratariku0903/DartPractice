336845301293c3051a2994dc9d8052beb71e9c0e
muratariku0903 <18ba179r@rikkyo.ac.jp>
2022-08-09 23:42:40 +0900

    update PasswordGenerator

--- tasks/PasswordGenerator.dart
+++ tasks/PasswordGenerator.dart
@@ -1,18 +1,34 @@
+import 'dart:convert';
 import 'dart:math';
 
 void main(List<String> args) {
-  String password = generatePassword(size: 20);
+  String password = generatePassword1(len: 20);
+  List<int> nums = List.generate(10, (index) => index);
+  print(nums);
+  generatePassword2();
   print(password);
 }
 
-String generatePassword({int size: 10}) {
+String generatePassword1({int len: 10}) {
+  if (len <= 0) throw ('Please set valid password len.');
+
   final random = Random();
   print(Random.secure());
   String password = '';
-  for (var i = 0; i < size; i++) {
+  for (var i = 0; i < len; i++) {
     int charCode = random.nextInt(124);
     password += String.fromCharCode(charCode);
   }
+
+  return password;
+}
+
+String generatePassword2({int len: 10}) {
+  final random = Random.secure();
+  List<int> randomNums = List.generate(len, (_) => random.nextInt(255));
+  List<String> randomStrs = base64UrlEncode(randomNums).split('').toList();
+  randomStrs.shuffle();
+  String password = randomStrs.join('');
 
   return password;
 }
